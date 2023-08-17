a4e0d008f0284a2aba8fb9610664632a189eac23
Mujojo03 <rosejaneforyou@gmail.com>
2023-08-17 13:01:47 -0700

    You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,16 +14,3 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		for (x = 0; x < n; x++)
-		{
-			for (y = 0; y < x; y++)
-			{
-		_putchar(32);
-			}
-	_putchar(92);
-	_putchar('\n');
-		}
-	}
-}	
-
-
