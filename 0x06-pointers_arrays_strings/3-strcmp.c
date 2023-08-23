7d674d0547213c52d0c52fd142efd8cccaaef4ac
Mujojo03 <rosejaneforyou@gmail.com>
2023-08-23 08:34:32 -0700

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,8 @@
  * @s1: a pointer to the first string to be compared to
  * @s2: a pointer to the second string to be compared to
  * Return: If srtl < strl2, the negative difference of the
- * first unmatched characters if str1 == str2, 0
+ * first unmatched characters
+ * if str1 == str2, 0
  * if str1 > str2, the positive difference of the first unmatched characters
  */
 int _strcmp(char *s1, char *s2)
