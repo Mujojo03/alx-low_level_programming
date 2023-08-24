71124de626831efe2385aec0280cd2a5b74db305
Mujojo03 <rosejaneforyou@gmail.com>
2023-08-24 09:33:30 -0700

    A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,17 +3,21 @@
 int main(void)
 {
 	int n;
-	int a[s];
+	int a[5];
 	int *p;
 
 	a[2] = 1024;
 	p = &n;
-	/*
-	 * write your line of code here...
-	 * oly one statement
-	 */
+  /*
+   * write your line of code here...
+   * Remember:
+   * you are not allowed to use a
+   * you are not allowed to modify
+   * only one statement
+   * you are not allowed to code anything else than this line of code
+   */
 	*(p + 5) = 98;
-	/*...so that thisprints 98\n */
+	/*...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
