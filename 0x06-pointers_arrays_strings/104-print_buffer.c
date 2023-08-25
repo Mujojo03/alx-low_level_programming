c9bd04b2c3978cff6c6c646f298f541a10228de8
Mujojo03 <rosejaneforyou@gmail.com>
2023-08-25 02:33:29 -0700

    function that prints a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -20,16 +20,16 @@ void print_buffer(char *b, int size)
 	while (x < size)
 	{
 		y = size - x < 10 ? size - x : 10;
-		printf("%08x: ", x);
+		printf("%08x:", x);
 		for (z = 0; z < 10; z++)
 		{
 			if (z < y)
 				printf("%02x", *(b + x + z));
 			else
-				printf(" ");
+				printf("  ");
 			if (z % 2)
 			{
-				printf(" ");
+				printf("  ");
 			}
 		}
 		for (z = 0; z < y; z++)
@@ -43,6 +43,6 @@ void print_buffer(char *b, int size)
 			printf("%c", c);
 		}
 		printf("\n");
-		z += 10;
+		x += 10;
 	}
 }
