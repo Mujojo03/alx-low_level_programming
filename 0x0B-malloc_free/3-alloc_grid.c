dd10bfa48d5b8f5c70c686ea1bb8288236ecf72e
Mujojo03 <rosejaneforyou@gmail.com>
2023-09-04 08:18:37 -0700

    function that returns a pointer to a 2 dimensional array of integers.

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - prints a grid of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: a pointer to two dimmensional array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **a;
+	int x, y;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	a = malloc(sizeof(int *) * height);
+
+	if (a == NULL)
+		return (NULL);
+	for (x = 0; x < height; x++)
+	{
+		a[x] = malloc(sizeof(int) * width);
+		if (a[x] == NULL)
+		{
+			for (; x >= 0; x--)
+				free(a[x]);
+			free(a);
+			return (NULL);
+		}
+	}
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+			a[x][y] = 0;
+	}
+	return (a)
+}
